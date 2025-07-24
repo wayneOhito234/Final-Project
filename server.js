bd5e3a5c2f632950a6c897292c7e1b5b02530290
+ <ohitowayneaol@gmail.com>
2025-07-24 15:40:53 +0300

    deploy on render

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create(); 
-const router = jsonServer.router("db.json");    
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`JSON Server is running on http://localhost:${port}`);    
+server.listen(3000, () => {
+  console.log('JSON Server is running');
 });
